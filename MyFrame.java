3aff9b7813cec15d64b0318700955a847de4734b
Gumiński Krzysztof <krzys.gumi@gmail.com>
2024-04-26 19:57:09 +0200

    Dodano minimalne wymiary ramki i przyciski w ToolPanel

--- MyFrame.java
+++ MyFrame.java
@@ -3,6 +3,7 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 public class MyFrame extends JFrame{
 
@@ -17,6 +18,7 @@ public class MyFrame extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(true);
         this.setSize(720, 405);
+        this.setMinimumSize(new Dimension(320, 220));
         this.setVisible(true);
         this.setLayout(new BorderLayout());
 
--- ToolPanel.java
+++ ToolPanel.java
@@ -1,13 +1,47 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ToolPanel extends JPanel {
-    
+
+    JButton start;
+    JButton end;
+    JButton shortest;
+
+    Font font = new Font( "Sans", Font.BOLD, 10);
+
     ToolPanel(){
 
-        this.setBackground(new Color(0,0,0));
-        this.setPreferredSize(new Dimension(50,50));
+        this.setBackground(Color.DARK_GRAY);
+        this.setPreferredSize(new Dimension(100,50));
+        //this.setLayout();
+
+        start = new JButton();
+        end = new JButton();
+        shortest = new JButton();
+
+        start.setText("Start");
+        end.setText("End");
+        shortest.setText("Shortest");
+
+        start.setFont(font);
+        end.setFont(font);
+        shortest.setFont(font);
+
+        start.setFocusable(false);
+        end.setFocusable(false);
+        shortest.setFocusable(false);
+
+        start.setPreferredSize(new Dimension(80, 30));
+        end.setPreferredSize(new Dimension(80, 30));
+        shortest.setPreferredSize(new Dimension(80, 30));
+
+        this.add(start);
+        this.add(end);
+        this.add(shortest);
     }
 }
